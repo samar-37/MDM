de33707d072d9f5cc15e2ed68c6f13eacce5e1b7
Samar Baikar <samarbaikar5@gmail.com>
2026-04-28 16:42:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
-    password = "12345"  # Vulnerability for testing
+    password = NONE  # Vulnerability for testing
     return {
         'statusCode': 200,
         'body': 'Hello Secure CI/CD'
